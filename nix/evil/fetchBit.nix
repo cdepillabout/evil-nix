9167f63e0a62f36b098c0689ec939ecadd6a9d71
(cdep)illabout <cdep.illabout@gmail.com>
2023-06-22 14:31:10 +0900

    update

--- nix/evil/fetchBit.nix
+++ nix/evil/fetchBit.nix
@@ -1,11 +1,33 @@
 
 { runCommand
-, downloadBitNum
-, collisions
-, drvNamePrefix ? ""
+, # Checkout ./downloadBitNum.nix and ./downloadFileSizeBitNum.nix for info
+  # about this argument.
+  downloadBitNum
+, # Checkout ./collisions.nix for info about this.
+  collisions
+, # A prefix for the driver name.
+  # This fetchBit derivation is used both for fetching bits corresponding
+  # to the file's contents, as well as the file's size.  Use this prefix
+  # to distinguish derivations.
+  drvNamePrefix ? ""
 }:
 
-url: urlHash: bitNum:
+# Return a derivation that fetches a single bit from a given URL, using
+# downloadBitNum to actually download the bit.  Create a single file that
+# contains either a "1" or a "0" to represent the value of the bit.
+
+
+# URL string to download.
+# Example: "https://raw.githubusercontent.com/cdepillabout/small-example-text-files/177c95e490cf44bcc42860bf0652203d3dc87900/hello-world.txt"
+url:
+
+# Hash of the URL to download.
+# Example: "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824"
+urlHash:
+
+# The bit number of the URL we will output.
+# Example: 12
+bitNum:
 
 let
   bitNumStr = toString bitNum;
@@ -20,6 +42,9 @@ runCommand
   "${drvNamePrefix'}bitvalue-${urlHash}-${bitNumStr}"
   {}
   ''
+    # Compare the contents of the PDF output by downloadBitNum.
+    # If downloadBitNum output the PDF for the bit value 1, then
+    # output a "1".  Otherwise, output a "0".
     if cmp -s "${bitNumResult}" "${bitValue1Pdf}"; then
       echo 1 > "$out"
     else
--- nix/evil/fetchByte.nix
+++ nix/evil/fetchByte.nix
@@ -3,11 +3,14 @@
 , runCommand
 }:
 
+# Similar to ./fetchBit.nix, but fetches a whole byte of the URL.
+
 url: urlHash: byteNum:
 
 let
   f = bitOffset: fetchBit url urlHash (byteNum * 8 + bitOffset);
 
+  # A list of 8 derivations, each of which fetches a single bit.
   l = builtins.genList f 8;
 in
 runCommand
@@ -17,13 +20,25 @@ runCommand
     allBitDrvs = l;
   }
   ''
+    # Read the list of derivations, each of which should contain a single file
+    # with a single ascii character "1" or "0".  Loop over files in this list,
+    # and write the individual bit characters to the file ./bitValues.
     (tr ' ' '\n' < "$allBitDrvsPath" ; echo) | while read -r line ; do
       tr --delete '\n' < "$line" >> ./bitValues
     done
 
+    # Read in the bit string that represents this bit we are trying to download.
+    # Example: "00100011"
     bits="$(cat ./bitValues)"
+
+    # Convert this bit string do a decimal number:
+    # Example: "35"
     decimal="$((2#$bits))"
+
+    # Convert this decimal number to a hex number:
+    # Example: "23"
     hex="$(printf '%x' $decimal)"
 
+    # Convert this hex number to a raw byte, and write it to a file.
     printf '%b' "\\x$hex" > $out
   ''
